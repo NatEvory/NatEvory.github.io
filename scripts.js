aa65a4eb41d501f94e339ae35801b3317966763f
Nat Evory <Nat.Evory@gmail.com>
2016-05-13 14:19:04 -0400

    Added resizing profile card and image

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-$(window).scroll(function(){
-	
-})
+
+(function(){
+	var $window = $(window);
+	var isBig = $window.scrollTop()!=0;
+	$window.scroll(function(){
+		var window_top = $window.scrollTop();
+		if(window_top > 0 && !isBig){
+			isBig = true;
+			$("#profile").switchClass("profile-card","profile-card-small");
+			$("#profile-image").switchClass("profile-image","profile-image-small");
+			$("#header").switchClass("top-padding","top-padding-small");
+		}
+		if(window_top == 0 && isBig) {
+			isBig = false;
+			$("#profile").switchClass("profile-card-small","profile-card");
+			$("#profile-image").switchClass("profile-image-small","profile-image");
+			$("#header").switchClass("top-padding-small","top-padding");
+		}
+	})
+})();
