1c4c7f14b2acc8d526c91a8f99baff7bd378cd4f
Nat Evory <Nat.Evory@gmail.com>
2016-05-13 14:23:05 -0400

    function refactor

--- scripts.js
+++ scripts.js
@@ -2,19 +2,28 @@
 (function(){
 	var $window = $(window);
 	var isBig = $window.scrollTop()!=0;
-	$window.scroll(function(){
+	$window.scroll(sizeProfile);
+
+	function sizeProfile(){
 		var window_top = $window.scrollTop();
 		if(window_top > 0 && !isBig){
 			isBig = true;
-			$("#profile").switchClass("profile-card","profile-card-small");
-			$("#profile-image").switchClass("profile-image","profile-image-small");
-			$("#header").switchClass("top-padding","top-padding-small");
+			shrinkProfile();
 		}
 		if(window_top == 0 && isBig) {
 			isBig = false;
-			$("#profile").switchClass("profile-card-small","profile-card");
-			$("#profile-image").switchClass("profile-image-small","profile-image");
-			$("#header").switchClass("top-padding-small","top-padding");
+			growProfile();
 		}
-	})
+	}
+
+	function shrinkProfile(){
+		$("#profile").switchClass("profile-card","profile-card-small");
+		$("#profile-image").switchClass("profile-image","profile-image-small");
+		$("#header").switchClass("top-padding","top-padding-small");
+	}
+	function growProfile(){
+		$("#profile").switchClass("profile-card-small","profile-card");
+		$("#profile-image").switchClass("profile-image-small","profile-image");
+		$("#header").switchClass("top-padding-small","top-padding");
+	}
 })();
