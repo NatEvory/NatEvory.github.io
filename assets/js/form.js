703af857251de0a1f951171f272db2a8bea63863
Nat Evory <nat.evory@gmail.com>
2021-10-28 16:08:26 -0400

    Contact form now submits to formspree.io

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,6 +3,8 @@
     var $window = $(window);
 
     var contactSubmitEndpoint = "https://formspree.io/f/xdoyvqab";
+    var simulateSubmit = false;
+    var simulateError = false;
 
     $window.on('load',function(){
         var $contactForm = $('#contactForm');
@@ -12,13 +14,43 @@
         var $contactMessage = $('#contactMessage');
         var $contactSubmit = $('#contactSubmit');
 
-        $contactForm.submit((e)=>{
-            e.preventDefault();
+        $contactForm.submit((event)=>{
+            event.preventDefault();
             $contactSubmit.addClass('disabled');
             $contactSubmit.prop('disabled',true);
+            var submissionData = {
+                name:$contactName.val(),
+                email:$contactEmail.val(),
+                company:$contactCompany.val(),
+                message:$contactMessage.val(),
 
+            }
+            submitForm(submissionData);
         })
 
+        function submitForm(data){
+            console.log("Pretending to submit form");
+            console.log(data);
+            if(simulateSubmit){
+                if(simulateError){
+                    onSubmitError(new Error("A simulated Error occurred!"))
+                } else {
+                    onSubmitSuccess("You did it hooray!");
+                }
+            } else {
+                $.post(contactSubmitEndpoint,data,onSubmitSuccess,'json');
+            }
+            
+        }
+
+        function onSubmitSuccess(){
+            console.log("GREAT SUCCESS!");
+
+        }
+        function onSubmitError(error){
+            console.log("FAILURE");
+            console.log(error);
+        }
     });
 
 
